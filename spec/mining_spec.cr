5327024add92b4a633806e7edbceb950f31db8d7
Wojciech Wieczorek <wwieczorek@ath.bielsko.pl>
2021-09-16 22:00:45 +0200

    Changed solver to Gurobi

--- spec/mining_spec.cr
+++ spec/mining_spec.cr
@@ -2,39 +2,146 @@ require "spec"
 require "../src/mining.cr"
 
 describe "Mining" do
-  it "uses the lp-solve MIP solver" do
-    lp = LibLpSolve.make_lp(0, 2)
-    LibLpSolve.set_verbose(lp, 0)
-    LibLpSolve.add_constraint(lp, [0.0, 2.0, 2.0].to_unsafe, Mining::GE, 3.0)
-    LibLpSolve.add_constraint(lp, [0.0, -2.0, 2.0].to_unsafe, Mining::LE, 3.0)
-    LibLpSolve.add_constraint(lp, [0.0, 4.0, 2.0].to_unsafe, Mining::LE, 19.0)
-    LibLpSolve.set_int(lp, 1, Mining::TRUE)
-    LibLpSolve.set_int(lp, 2, Mining::TRUE)
-    LibLpSolve.set_lowbo(lp, 1, 0.0)
-    LibLpSolve.set_lowbo(lp, 2, 0.0)
-    LibLpSolve.set_obj_fn(lp, [0.0, 1.0, 1.0].to_unsafe)
-    LibLpSolve.set_maxim(lp)
-    LibLpSolve.solve(lp)
-    status = LibLpSolve.get_status(lp)
-    status.should eq(Mining::OPTIMAL)
-    result = Array(Float64).new(2, 0.0)
-    LibLpSolve.get_variables(lp, result.to_unsafe)
-    result.should eq([3, 3])
-    profit = LibLpSolve.get_objective(lp)
-    LibLpSolve.delete_lp(lp)
-    profit.should eq(6) 
+  it "implements GA with pmx operator" do
+    ga = Mining::Ga.new(3, 10)
+    ga.population[0][0, 10] = [8, 4, 7, 3, 6, 2, 5, 1, 9, 0]
+    ga.population[1][0, 10] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    ga.pmx(ga.population[0], ga.population[1], ga.population[2], true)
+    ga.population[2].should eq([0, 7, 4, 3, 6, 2, 5, 1, 8, 9])
+    ga.pmx(ga.population[0], ga.population[1], ga.population[2])
+    ga.population[2].sort.should eq([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+  end
+
+  it "can use GA for solving TSP" do
+    dist = [[  0.0, 132.0, 217.0, 164.0,  58.0],
+            [132.0,   0.0, 290.0, 201.0,  79.0],
+            [217.0, 290.0,   0.0, 113.0, 303.0],
+            [164.0, 201.0, 113.0,   0.0, 196.0],
+            [ 58.0,  79.0, 303.0, 196.0,   0.0]]
+    ga = Mining::Ga.new(5, 5)
+    eval = ->(path : Array(Int32)) {
+      cost = 0.0
+      (0..4).each do |i|
+        cost += dist[path[i]][path[(i+1) % 5]]
+      end
+      cost
+    }
+    ga.fitness_fun = eval
+    ga.run
+    ga.best_error.should eq(668)
+  end
+
+  it "uses Gurobi MIP solver" do
+    env = 0_u64
+    model = 0_u64
+    error = 0
+    sol = Array(Float64).new(2, 0.0)
+    ind = Array(Int32).new(2, 0)
+    val = Array(Float64).new(2, 0.0)
+    obj = Array(Float64).new(2, 0.0)
+    vtype = Array(UInt8).new(2, 0_u8)
+    optimstatus = 0
+    objval = 0.0
+    error = LibGrb.GRBemptyenv(pointerof(env))
+    if error != 0
+      message = LibGrb.GRBgeterrormsg(env)
+      raise "ERROR: #{String.new(message)}\n"
+    end
+    error = LibGrb.GRBsetstrparam(env, "LogFile", "/run/shm/grb.log")
+    if error != 0
+      message = LibGrb.GRBgeterrormsg(env)
+      raise "ERROR: #{String.new(message)}\n"
+    end
+    error = LibGrb.GRBstartenv(env)
+    if error != 0
+      message = LibGrb.GRBgeterrormsg(env)
+      raise "ERROR: #{String.new(message)}\n"
+    end
+    error = LibGrb.GRBnewmodel(env, pointerof(model), "mip1", 0, nil, nil, nil, nil, 0)
+    if error != 0
+      message = LibGrb.GRBgeterrormsg(env)
+      raise "ERROR: #{String.new(message)}\n"
+    end
+    obj[0] = 1.0
+    obj[1] = 1.0
+    vtype[0] = Mining::INTEGER
+    vtype[1] = Mining::INTEGER
+    error = LibGrb.GRBaddvars(model, 2, 0, nil, nil, nil, obj.to_unsafe,
+                      [0.0, 0.0].to_unsafe, [20.0, 20.0].to_unsafe,
+                      vtype.to_unsafe, 0)
+    if error != 0
+      message = LibGrb.GRBgeterrormsg(env)
+      raise "ERROR: #{String.new(message)}\n"
+    end
+    error = LibGrb.GRBsetintattr(model, "ModelSense", Mining::GRB_MAXIMIZE)
+    if error != 0
+      message = LibGrb.GRBgeterrormsg(env)
+      raise "ERROR: #{String.new(message)}\n"
+    end
+    ind[0] = 0
+    ind[1] = 1
+    val[0] = 2.0
+    val[1] = 2.0
+    error = LibGrb.GRBaddconstr(model, 2, ind.to_unsafe, val.to_unsafe, Mining::GE, 3.0, "c0")
+    if error != 0
+      message = LibGrb.GRBgeterrormsg(env)
+      raise "ERROR: #{String.new(message)}\n"
+    end
+    ind[0] = 0
+    ind[1] = 1
+    val[0] = -2.0
+    val[1] = 2.0
+    error = LibGrb.GRBaddconstr(model, 2, ind.to_unsafe, val.to_unsafe, Mining::LE, 3.0, "c1")
+    if error != 0
+      message = LibGrb.GRBgeterrormsg(env)
+      raise "ERROR: #{String.new(message)}\n"
+    end
+    ind[0] = 0
+    ind[1] = 1
+    val[0] = 4.0
+    val[1] = 2.0
+    error = LibGrb.GRBaddconstr(model, 2, ind.to_unsafe, val.to_unsafe, Mining::LE, 19.0, "c2")
+    if error != 0
+      message = LibGrb.GRBgeterrormsg(env)
+      raise "ERROR: #{String.new(message)}\n"
+    end
+    error = LibGrb.GRBoptimize(model)
+    if error != 0
+      message = LibGrb.GRBgeterrormsg(env)
+      raise "ERROR: #{String.new(message)}\n"
+    end
+    error = LibGrb.GRBgetintattr(model, "Status", pointerof(optimstatus))
+    if error != 0
+      message = LibGrb.GRBgeterrormsg(env)
+      raise "ERROR: #{String.new(message)}\n"
+    end
+    optimstatus.should eq(Mining::GRB_OPTIMAL)
+    error = LibGrb.GRBgetdblattrarray(model, "X", 0, 2, sol.to_unsafe)
+    if error != 0
+      message = LibGrb.GRBgeterrormsg(env)
+      raise "ERROR: #{String.new(message)}\n"
+    end
+    sol.should eq([3, 3])
+    error = LibGrb.GRBgetdblattr(model, "ObjVal", pointerof(objval))
+    if error != 0
+      message = LibGrb.GRBgeterrormsg(env)
+      raise "ERROR: #{String.new(message)}\n"
+    end
+    LibGrb.GRBfreemodel(model)
+    LibGrb.GRBfreeenv(env)
+    objval.should eq(6)
   end
   
   it "loads decision table from csv file" do
     tab = Mining.loadTable("./spec/data.csv")
     tab.size.should eq(4)
-    tab[2].should eq(["Jeff Smith", "2018", "Prescott House", "17-D", "3.20"])
+    tab[2].should eq(["3.20", "Jeff Smith", "2018", "Prescott House", "17-D"])
   end
   
   it "finds all descriptors from a decision table" do
     tab = [["0", "b", "a"], ["1", "c", "a"], ["1", "a", "b"], ["0", "c", "b"]]
     dict = Mining.findAllDescriptors(tab)
-    dict.should eq({ {1, "b"} => 1, {2, "a"} => 2, {1, "c"} => 3, {1, "a"} => 4, {2, "b"} => 5 })
+    dict.should eq({ {1, "b"} => 0, {2, "a"} => 1, {1, "c"} => 2, {1, "a"} => 3, {2, "b"} => 4 })
   end
   
   it "finds minimum test set" do
@@ -45,7 +152,7 @@ describe "Mining" do
            ["1", "e", "f", "e", "f"]]
     dict = Mining.findAllDescriptors(tab)
     result = Mining.findMinTestSet(tab, dict)
-    result.should eq(Set{ {1, "e"}, {3, "f"}, {3, "g"} })
+    result.size.should eq(3)
   end
   
   it "generates a decision tree" do
@@ -53,11 +160,11 @@ describe "Mining" do
     dict = Mining.findAllDescriptors(tab)
     mts = Mining.findMinTestSet(tab, dict)
     root = Mining.buildTree((0...tab.size).to_set, mts, tab)
-    root.question.should eq({1, "0"})
+    root.question.should eq({3, "0"})
     root.yes.as(Mining::Node).decision.should eq("b")
-    root.no.as(Mining::Node).question.should eq({3, "0"})
-    root.no.as(Mining::Node).yes.as(Mining::Node).decision.should eq("b")
-    root.no.as(Mining::Node).no.as(Mining::Node).decision.should eq("a")
+    root.no.as(Mining::Node).question.should eq({1, "1"})
+    root.no.as(Mining::Node).yes.as(Mining::Node).decision.should eq("a")
+    root.no.as(Mining::Node).no.as(Mining::Node).decision.should eq("b")
   end
 
   it "can classify new objects" do
@@ -71,5 +178,15 @@ describe "Mining" do
           19 => "e", 20 => "w", 21 => "v", 22 => "p"}
     result = Mining.classify ob, with: root
     result.should eq("p")
+  end
+
+  it "generates a decision tree for breast" do
+    tab = Mining.loadTable("./spec/balance-scale_tr.csv")
+    dict = Mining.findAllDescriptors(tab)
+    mts = Mining.findMinTestSet(tab, dict)
+    root = Mining.buildTree((0...tab.size).to_set, mts, tab)
+    puts
+    root.display
+    puts
   end
 end
--- src/mining.cr
+++ src/mining.cr
@@ -1,57 +1,169 @@
-@[Link(ldflags: "/usr/lib/liblpsolve55.so")]
+@[Link(ldflags: "/home/wojtekw/gurobi912/linux64/lib/libgurobi91.so")]
 
-lib LibLpSolve
-  fun make_lp(rows : Int32, columns : Int32) : UInt64
-  fun delete_lp(lprec : UInt64) : Void
-  fun get_status(lprec : UInt64) : Int32
-  fun get_statustext(lprec : UInt64, statuscode : Int32) : UInt8*
-  fun add_constraint(lprec : UInt64, row : Float64*, constr_type : Int32, rh : Float64) : UInt8
-  fun set_obj_fn(lprec : UInt64, row : Float64*) : UInt8
-  fun set_maxim(lprec : UInt64) : Void
-  fun set_minim(lprec : UInt64) : Void
-  fun set_bounds(lprec : UInt64, colnr : Int32, lower : Float64, upper : Float64) : UInt8
-  fun set_unbounded(lprec : UInt64, colnr : Int32) : UInt8
-  fun set_upbo(lprec : UInt64, colnr : Int32, value : Float64) : UInt8
-  fun set_lowbo(lprec : UInt64, colnr : Int32, value : Float64) : UInt8
-  fun set_int(lprec : UInt64, colnr : Int32, must_be_int : UInt8) : UInt8
-  fun set_binary(lprec : UInt64, colnr : Int32, must_be_bin : UInt8) : UInt8
-  fun solve(lprec : UInt64) : Int32
-  fun get_variables(lprec : UInt64, var : Float64*) : UInt8
-  fun get_objective(lprec : UInt64) : Float64
-  fun set_verbose(lprec : UInt64, verbose : Int32) : Void
+lib LibGrb
+  fun GRBemptyenv(envP : UInt64*) : Int32
+  fun GRBsetstrparam(env : UInt64, paramname : UInt8*, value : UInt8*) : Int32
+  fun GRBstartenv(env : UInt64) : Int32
+  fun GRBnewmodel(env : UInt64, modelP : UInt64*, name : UInt8*, 
+                  numvars : Int32, obj : Float64*, lb : Float64*, 
+                  ub : Float64*, vtype : UInt8*, varnames : UInt64) : Int32
+  fun GRBaddvars(model : UInt64, numvars : Int32, numnz : UInt64,
+                 vbeg : UInt64*, vind : Int32*, vval : Float64*,
+                 obj : Float64*, lb : Float64*, ub : Float64*, vtype : UInt8*,
+                 varnames : UInt64) : Int32
+  fun GRBsetintattr(model : UInt64, attrname : UInt8*, newvalue : Int32) : Int32
+  fun GRBaddconstr(model : UInt64, numnz : Int32, cind : Int32*, cval : Float64*,
+                   sense : UInt8, rhs : Float64, constrname : UInt8*) : Int32
+  fun GRBoptimize(model : UInt64) : Int32
+  fun GRBgetintattr(model : UInt64, attrname : UInt8*, valueP : Int32*) : Int32
+  fun GRBgetdblattr(model : UInt64, attrname : UInt8*, valueP : Float64*) : Int32
+  fun GRBgetdblattrarray(model : UInt64, attrname : UInt8*, first : Int32,
+                         len : Int32, values : Float64*) : Int32
+  fun GRBgeterrormsg(env : UInt64) : UInt8*
+  fun GRBfreemodel(model : UInt64) : Int32
+  fun GRBfreeenv(env : UInt64) : Void
 end
 
 require "csv"
 
 module Mining
   extend self
-  FALSE = 0_u8
-  TRUE = 1_u8
-  LE = 1
-  EQ = 3
-  GE = 2
-
-  # Solver status values
-  UNKNOWNERROR  = -5
-  DATAIGNORED   = -4
-  NOBFP         = -3
-  NOMEMORY      = -2
-  NOTRUN        = -1
-  OPTIMAL       =  0
-  SUBOPTIMAL    =  1
-  INFEASIBLE    =  2
-  UNBOUNDED     =  3
-  DEGENERATE    =  4
-  NUMFAILURE    =  5
-  USERABORT     =  6
-  TIMEOUT       =  7
-  RUNNING       =  8
-  PRESOLVED     =  9
-  ACCURACYERROR = 25
+  GRB_MINIMIZE = 1
+  GRB_MAXIMIZE = -1
+  BINARY     = 'B'.ord.to_u8
+  CONTINUOUS = 'C'.ord.to_u8
+  INTEGER    = 'I'.ord.to_u8
+  SEMICONT   = 'S'.ord.to_u8
+  SEMIINT    = 'N'.ord.to_u8
+  GE = '>'.ord.to_u8
+  LE = '<'.ord.to_u8
+  EQ = '='.ord.to_u8
+  
+  # Model status codes
+  GRB_LOADED          =  1
+  GRB_OPTIMAL         =  2
+  GRB_INFEASIBLE      =  3
+  GRB_INF_OR_UNBD     =  4
+  GRB_UNBOUNDED       =  5
+  GRB_CUTOFF          =  6
+  GRB_ITERATION_LIMIT =  7
+  GRB_NODE_LIMIT      =  8
+  GRB_TIME_LIMIT      =  9
+  GRB_SOLUTION_LIMIT  = 10
+  GRB_INTERRUPTED     = 11
+  GRB_NUMERIC         = 12
+  GRB_SUBOPTIMAL      = 13
+  GRB_INPROGRESS      = 14
+  GRB_USER_OBJ_LIMIT  = 15
 
   alias Descriptor = Tuple(Int32, String)
   alias Table = Array(Array(String))
+
+  class Ga
+    getter population : Array(Array(Int32))
+    getter pop_size : Int32
+    getter genotype_len : Int32
+    getter best_individual : Array(Int32)
+    getter best_error : Float64
+    setter fitness_fun : Array(Int32) -> Float64
+    setter tournament_size : Int32
+    setter n_iterations : Int32
   
+    # An individual is the permutation of [0, 1, ..., genotype_len - 1]
+    def initialize(@pop_size, @genotype_len)
+      raise "Population size have to be >= 3" if pop_size < 3
+      @population = Array(Array(Int32)).new(pop_size) do |i|
+        (0...genotype_len).to_a.shuffle!
+      end
+      @best_individual = (0...genotype_len).to_a
+      @best_error = Float64::INFINITY
+      @fitness_fun = ->(arr : Array(Int32)) { 0.0 }
+      @tournament_size = 3
+      @n_iterations = 200
+    end
+  
+    private def reservoirSample(s, r, k)
+      # fill the reservoir array
+      (0...k).each { |i| r[i] = i }
+      # replace elements with gradually decreasing probability
+      (k..s).each do |i|
+        j = rand(i + 1)
+        if j < k
+          r[j] = i
+        end
+      end
+    end
+  
+    private def not_in(x, arr, first, last)
+      (first..last).each do |i|
+        return false if arr[i] == x
+      end
+      return true
+    end
+  
+    private def idx_for(x, arr)
+      arr.each_with_index do |y, i|
+        return i if x == y
+      end
+      raise "#{x} not found in #{arr}"
+      0
+    end
+  
+    def pmx(arr1 : Array(Int32), arr2 : Array(Int32), arr3 : Array(Int32), test = false)
+      n = arr1.size
+      point = StaticArray[0, 0]
+      reservoirSample(n, point.to_unsafe, 2)
+      point[0], point[1] = point[1], point[0] if point[0] > point[1]
+      point[0], point[1] = 3, 8 if test
+      (0...point[0]).each { |i| arr3[i] = -1 }
+      (point[0]...point[1]).each { |i| arr3[i] = arr1[i] }
+      (point[1]...n).each { |i| arr3[i] = -1 }
+      (point[0]...point[1]).each do |k|
+        if not_in(arr2[k], arr1, point[0], point[1] - 1)
+          target_idx = idx_for(arr1[k], arr2)
+          while target_idx >= point[0] && target_idx < point[1]
+            target_idx = idx_for(arr1[target_idx], arr2)
+          end
+          arr3[target_idx] = arr2[k]
+        end
+      end
+      (0...point[0]).each do |i|
+        arr3[i] = arr2[i] if arr3[i] == -1
+      end
+      (point[1]...n).each do |i|
+        arr3[i] = arr2[i] if arr3[i] == -1
+      end
+    end
+  
+    def run
+      error = @population.map { |arr| @fitness_fun.call(arr) }
+      best_idx = (0...@pop_size).min_by { |i| error[i] }
+      @best_error = error[best_idx]
+      @best_individual[0, @genotype_len] = @population[best_idx]
+      iteration = 0
+      tournament = Array(Int32).new(@tournament_size, 0)
+      while iteration < @n_iterations && @best_error > 0
+        reservoirSample(@pop_size - 1, tournament, @tournament_size)
+        tournament.sort_by! { |idx| error[idx] }
+        parent1 = population[tournament[0]]
+        parent2 = population[tournament[1]]
+        child = population[tournament[-1]]
+        pmx(parent1, parent2, child)
+        if rand < 0.01
+          point = StaticArray[0, 0]
+          reservoirSample(@genotype_len - 1, point.to_unsafe, 2)
+          child.swap(point[0], point[1])
+        end
+        error[tournament[-1]] = @fitness_fun.call(child)
+        if error[tournament[-1]] < @best_error
+          @best_error = error[tournament[-1]]
+          @best_individual[0, @genotype_len] = @population[tournament[-1]]
+        end
+        iteration += 1
+      end
+    end
+  end
+
   def loadTable(filename : String)
     content = File.read(filename)
     table = [] of Array(String)
@@ -59,7 +171,7 @@ module Mining
     csv = CSV.new(content, headers: false, strip: true) 
     while csv.next
       arr = csv.row.to_a 
-      # arr.rotate!(arr.size - 1)
+      arr.rotate!(arr.size - 1)
       if !row_set_without_decision.add(arr[1..])
         puts "Already in table."
       else
@@ -75,57 +187,124 @@ module Mining
     tab.each do |row|
       row.each_with_index do |s, i|
         if i > 0 && !dict.has_key?({i, s})
-          idx += 1
           dict[{i, s}] = idx
+          idx += 1
         end
       end
     end
     dict
   end
 
-  def encodeProblem(tab : Table, dict : Hash(Descriptor, Int32)) : UInt64
+  def encodeProblem(tab : Table, dict : Hash(Descriptor, Int32))
     ncols = dict.size
-    lp = LibLpSolve.make_lp(0, ncols)
-    LibLpSolve.set_verbose(lp, 0)
-    (1..ncols).each { |j| LibLpSolve.set_binary(lp, j, TRUE) }
+    env = 0_u64
+    model = 0_u64
+    error = 0
+    ind = Array(Int32).new(ncols, 0)
+    val = Array(Float64).new(ncols, 0.0)
+    obj = Array(Float64).new(ncols, 1.0)
+    vtype = Array(UInt8).new(ncols, BINARY)
+    # Create environment
+    error = LibGrb.GRBemptyenv(pointerof(env))
+    if error != 0
+      message = LibGrb.GRBgeterrormsg(env)
+      raise "ERROR: #{String.new(message)}\n"
+    end
+    error = LibGrb.GRBsetstrparam(env, "LogFile", "/run/shm/grb.log")
+    if error != 0
+      message = LibGrb.GRBgeterrormsg(env)
+      raise "ERROR: #{String.new(message)}\n"
+    end
+    error = LibGrb.GRBstartenv(env)
+    if error != 0
+      message = LibGrb.GRBgeterrormsg(env)
+      raise "ERROR: #{String.new(message)}\n"
+    end
+    # Create an empty model
+    error = LibGrb.GRBnewmodel(env, pointerof(model), "mip1", 0, nil, nil, nil, nil, 0)
+    if error != 0
+      message = LibGrb.GRBgeterrormsg(env)
+      raise "ERROR: #{String.new(message)}\n"
+    end
+    # Add variables
+    error = LibGrb.GRBaddvars(model, ncols, 0, nil, nil, nil, obj.to_unsafe, 
+                              nil, nil, vtype.to_unsafe, 0)
+    if error != 0
+      message = LibGrb.GRBgeterrormsg(env)
+      raise "ERROR: #{String.new(message)}\n"
+    end
+    # Change objective sense to minimization
+    error = LibGrb.GRBsetintattr(model, "ModelSense", GRB_MINIMIZE)
+    if error != 0
+      message = LibGrb.GRBgeterrormsg(env)
+      raise "ERROR: #{String.new(message)}\n"
+    end
     nrows = tab.size
     nattr = tab[0].size
+    cnum = 0
     (0..nrows-2).each do |i|
       (i+1..nrows-1).each do |j|
         if tab[i][0] != tab[j][0]  # different decision classes
-          ineq = Array(Float64).new(ncols + 1, 0.0)
+          numnz = 0
           (1...nattr).each do |k|
             if tab[i][k] != tab[j][k]
-              ineq[dict[{k, tab[i][k]}]] = 1.0
-              ineq[dict[{k, tab[j][k]}]] = 1.0
+              ind[numnz] = dict[{k, tab[i][k]}]
+              val[numnz] = 1.0
+              numnz += 1
+              ind[numnz] = dict[{k, tab[j][k]}]
+              val[numnz] = 1.0
+              numnz += 1
             end
           end
-          LibLpSolve.add_constraint(lp, ineq.to_unsafe, GE, 1.0)
+          error = LibGrb.GRBaddconstr(model, numnz, ind.to_unsafe, val.to_unsafe, GE, 1.0, "c#{cnum}")
+          cnum += 1
+          if error != 0
+            message = LibGrb.GRBgeterrormsg(env)
+            raise "ERROR: #{String.new(message)}\n"
+          end
         end
       end
     end
-    c = Array(Float64).new(ncols + 1, 1.0)
-    c[0] = 0.0
-    LibLpSolve.set_obj_fn(lp, c.to_unsafe)
-    LibLpSolve.set_minim(lp)
-    lp
+    {env, model}
   end
 
   def findMinTestSet(tab : Table, dict : Hash(Descriptor, Int32)) : Set(Descriptor)
-    lp = encodeProblem(tab, dict)
-    LibLpSolve.solve(lp)
-    status = LibLpSolve.get_status(lp)
-    if status != OPTIMAL
-      message = LibLpSolve.get_statustext(lp, status)
-      raise "Lp-solve status: " + String.new(message)
+    env, model = encodeProblem(tab, dict)
+    optimstatus = 0
+    objval = 0.0
+    # Optimize model
+    error = LibGrb.GRBoptimize(model)
+    if error != 0
+      message = LibGrb.GRBgeterrormsg(env)
+      raise "ERROR: #{String.new(message)}\n"
+    end
+    # Capture solution information
+    error = LibGrb.GRBgetintattr(model, "Status", pointerof(optimstatus))
+    if error != 0
+      message = LibGrb.GRBgeterrormsg(env)
+      raise "ERROR: #{String.new(message)}\n"
+    end
+    if optimstatus != GRB_OPTIMAL
+      if optimstatus == GRB_INF_OR_UNBD
+        raise "Model is infeasible or unbounded"
+      else
+        raise "Optimization was stopped early"
+      end
     end
     ncols = dict.size
-    vars = Array(Float64).new(ncols + 1, 0.0)
-    LibLpSolve.get_variables(lp, vars.to_unsafe + 1)
-    LibLpSolve.delete_lp(lp)
+    sol = Array(Float64).new(ncols, 0.0)
+    error = LibGrb.GRBgetdblattrarray(model, "X", 0, ncols, sol.to_unsafe)
+    if error != 0
+      message = LibGrb.GRBgeterrormsg(env)
+      raise "ERROR: #{String.new(message)}\n"
+    end
+    # Free model
+    LibGrb.GRBfreemodel(model)
+    # Free environment
+    LibGrb.GRBfreeenv(env)
     result = Set(Descriptor).new
     dict.each do |d, i|
-      result.add(d) if vars[i] == 1
+      result.add(d) if sol[i] == 1
     end
     result
   end
@@ -214,7 +393,7 @@ module Mining
       if object.has_key?(attr)
         node = object[attr] == value ? node.yes.as(Node) : node.no.as(Node)
       else
-        return "?"
+        node = node.no.as(Node)
       end
     end
     node.decision
